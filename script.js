83b8cf85bf73b0cf4162bc1c6c637a2eb01ce481
Dominik <rusac.dominik@gmail.com>
2020-06-16 22:05:24 +0200

    formatting

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function filterPosts(e) {
         const title = post.querySelector('.post-title').innerText.toUpperCase();
         const body = post.querySelector('.post-body').innerText.toUpperCase();
 
-        if(title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+        if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
             post.style.display = 'flex';
         } else {
             post.style.display = 'none';
